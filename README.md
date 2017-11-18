0e2fc3e4e9320403a0e81d9a5f7632b3d79bd54a
KangWang <415249614@qq.com>
2017-11-18 17:53:24 +0800

    【添加题目】 如何让一段程序并发的执行，并最终汇总结果？

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # concurrent_practice
 java, demos of concurrent programming.
+
+##题目
+
+- 规定线程执行的次序。
+- 构建死锁出现的场景。
+- 如何让一段程序并发的执行，并最终汇总结果？
+- 如何中断一个正在运行的线程？
+- 线程延时执行某个任务？
+
+
---
+++ src/wang/demos/CollectThreadResults.java
@@ -0,0 +1,60 @@
+package wang.demos;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+
+/*
+ * 【题目】 如何让一段程序并发的执行，并最终汇总结果？
+ * 两个线程往一个公共集合中放入数字，放入结束后，则主线程从公共集合中取出数字求和。
+ * 
+ * 【分析及解答】
+ * 分治策略 ：主线程 负责汇总各线程的计算结果，其他线程负责计算且相互独立。
+ * 所以，主线程应该监视其他线程的执行状态。
+ * 
+ * 可以使用的java 并发工具：CyclicBarrier 和CountDownLatch
+ */
+public class CollectThreadResults {
+	private static List<Integer> results = new LinkedList<>(); //用来存储线程的执行结果。
+	private static final CountDownLatch latch = new CountDownLatch(2); // 用来控制主线程适当时候开始流程。 
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread T1 = new Thread(new MyThread(1, 2));
+		Thread T2 = new Thread(new MyThread(3,2));
+		T1.start();
+		T2.start();
+		Thread.sleep(600);
+		latch.await();
+		//结果应为10.
+		int sum = 0;
+		for(int result : results){
+			sum += result;
+		}
+		System.out.println("结果为 ： " + sum);
+	}
+	public  static class MyThread implements Runnable{
+		private int start;
+		private int count;
+		//从start 开始，放入count个数字。
+		public MyThread(int start,int count){
+			this.start = start;
+			this.count = count;
+		}
+		@Override
+		public void run() {
+			for(int i = 0; i < count;i++){
+				synchronized (results) {
+					results.add(start +i);
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			latch.countDown();
+		}
+	}
+}
